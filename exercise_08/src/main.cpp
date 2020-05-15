6434d8b85607204d8ec36d72be00f17e45769b60
Petar Popov <p.dim.popov@gmail.com>
2020-05-15 11:31:53 +0300

    exercise 8 finished

--- exercise_08/src/main.cpp
+++ exercise_08/src/main.cpp
@@ -1,9 +1,101 @@
 #include <Arduino.h>
+#define RED_LED 13
+#define GREEN_LED 12
+#define is ==
+#define TURN_ON_LED(LED) digitalWrite(LED, HIGH)
+#define TURN_OFF_LED(LED) digitalWrite(LED, LOW)
+#define RED_LED_BUTTON 2
+#define GREEN_LED_BUTON 3
+#define SERIAL_BAUD 9600
+#define TRACE_LINE(str) Serial.println(str)
+#define TRACE(str) Serial.print(str)
+
+int RedLedTurnOnTime = 0;
+int GreenLedTurnOnTime = 0;
+
+int RedLedLastState = 0;
+int GreenLedLastState = 0;
+
+void PrintChanges(const int redLedState, const int greenLedState);
+void OnRedLedBtnClick();
+void OnGreenLedBtnClick();
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+
+  pinMode(RED_LED_BUTTON, INPUT);
+  pinMode(GREEN_LED_BUTON, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(RED_LED_BUTTON), OnRedLedBtnClick, RISING);
+  attachInterrupt(digitalPinToInterrupt(GREEN_LED_BUTON), OnGreenLedBtnClick, FALLING);
+
+  Serial.begin(SERIAL_BAUD);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int redLedState = digitalRead(RED_LED);
+  int greenLedState = digitalRead(GREEN_LED);
+
+  if (RedLedLastState != redLedState || GreenLedLastState != greenLedState)
+  {
+    PrintChanges(redLedState, greenLedState);
+    RedLedLastState = redLedState;
+    GreenLedLastState = greenLedState;
+  }
+  
+}
+
+void PointSeven(const int led){
+  if (digitalRead(led))
+  {
+    TURN_OFF_LED(led);
+    return;
+  }
+  TURN_ON_LED(led);
+}
+
+void PointEight(const int button){
+  if (button is RED_LED_BUTTON)
+  {
+    if (!digitalRead(RED_LED))
+    {
+      TURN_ON_LED(RED_LED);
+      return;
+    } 
+
+    TURN_ON_LED(GREEN_LED);
+    return;
+  }
+  
+  if (button is GREEN_LED_BUTON)
+  {
+    if (digitalRead(GREEN_LED))
+    {
+      TURN_OFF_LED(GREEN_LED);
+      return;
+    }
+
+    TURN_OFF_LED(RED_LED);
+  }
+}
+
+void OnRedLedBtnClick(){
+  // PointSeven(RED_LED);
+
+  PointEight(RED_LED_BUTTON);
+}
+
+void OnGreenLedBtnClick(){
+  // PointSeven(GREEN_LED);
+
+  PointEight(GREEN_LED_BUTON);
+}
+
+void PrintChanges(const int redLedState, const int greenLedState){
+  TRACE_LINE("STATES: [RED_LED], [GREEN_LED]");
+  TRACE("        "); 
+  TRACE(redLedState ? "ON" : "OFF"); 
+  TRACE(", "); 
+  TRACE_LINE(greenLedState ? "ON" : "OFF");
 }
